6627532b73540ec3ffb1c3b707cbaf935cceca22
eo_01@protonmail.com <eo_01@protonmail.com>
2018-04-15 01:09:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+'use strict';
+
 function theBeatlesPlay (musicians, instruments){
-var allPlayers = [];
-for var (i = 0); i < musicians.length; i++){
-  array.push(musicians[i] + "plays" + instruments[i]);
+var phrases = [];
+for (var i = 0; i < musicians.length; i++){
+  phrases.push(musicians[i] + " plays " + instruments[i]);
 }
-return array;
+return phrases;
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+return array
+}
+function iLoveTheBeatles(num){
+  var phrases = [];
+  do{
+    phrases.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+  return phrases;
 }
